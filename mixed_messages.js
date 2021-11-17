6a485b943aad71ff0959ad673ce027c5e428421f
alangodwin <agodwin88@googlemail.com>
2021-11-17 22:35:30 +0000

    new outcomes new opportunities

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,18 +1,18 @@
 const horroscopeContents = {
-    startSentences: ['Perhaps', 'Greeting','There may be', 'Meeting'],
-    middleSentence: ['a stranger', 'a new love who'],
-    endSentence: ['will help you reach your goals', 'causes a major upset']
+    startSentences: ['Perhaps forgetting', 'Greeting','Today', 'Meeting', 'Putting to oneside' ],
+    middleSentence: ['a stranger', 'a new love', 'Jupiter', 'Mars', 'your friends' ],
+    endSentence: ['helps you reach your goals', 'causes a major upset', 'leads to new opportunities', 'can be the start of something better']
 }
 
 const randomPart = (object, sentencePart) => {
     return object[sentencePart][Math.floor(Math.random()* object[sentencePart].length)]
 };
 
-const fortune = (starSign='virgo') => {
+const fortune = (starSign='Virgo') => {
     let start = randomPart(horroscopeContents, 'startSentences') 
     let middle = randomPart(horroscopeContents, 'middleSentence')
     let end = randomPart(horroscopeContents, 'endSentence')
-    return `${start} ${middle} ${end}`
+    return `${starSign}: ${start} ${middle} ${end}.`
 }
 console.log(fortune())
 //console.log(randomPart(horroscopeContents, 'startSentences'))
